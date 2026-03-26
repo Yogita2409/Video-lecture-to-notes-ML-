7c148a70cfe4c0159951dbf753e23e41cc31dfc1
Yogita2409 <yogita03526@gmail.com>
2026-03-26 22:03:45 +0530

    fixed audio extraction issue

--- video_utils.py
+++ video_utils.py
@@ -3,13 +3,20 @@ import os
 
 def extract_audio(video_path):
     try:
-        video = VideoFileClip(video_path)
         audio_path = "audio.wav"
-        
-        video.audio.write_audiofile(audio_path)
-        
+
+        video = VideoFileClip(video_path)
+
+        # 🔥 Important fix
+        if video.audio is None:
+            print("No audio track found!")
+            return None
+
+        video.audio.write_audiofile(audio_path, codec='pcm_s16le')
+
         return os.path.abspath(audio_path)
 
     except Exception as e:
         print("Error in audio extraction:", e)
         return None
+    
